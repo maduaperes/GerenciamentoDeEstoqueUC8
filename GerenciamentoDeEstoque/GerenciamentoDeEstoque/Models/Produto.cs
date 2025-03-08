6e5dda485121ed446570f574d1d6620fd1c1bf91
Leozitos96 <marginaaladoleo@gmail.com>
2025-03-08 16:24:05 -0300

    Criação do banco de dados e implementação dos métodos
    # Diretórios Criados
    - `Database`
    - `Models`
    
    # Classes Criadas
    - `Database/Database`
    - `Models/Cliente`
    - `Models/Produto`
    - `Models/Usuario`
    
    # Métodos Implementados em `Database`:
    - **`CriarTabelasNoBanco`**: Cria as seguintes tabelas:
      - `Produtos` (Id, Nome, Codigo, Preco, Quantidade)
      - `Usuarios` (Id, Nome, Cargo, Senha)
      - `Pedidos` (Id, ClienteCPF, ClienteNome, Total)
      - `Carrinho` (Id, ProdutoId, PedidoId, Quantidade)
    
    - **`CadastrarUsuario`**: Método para cadastrar um novo usuário.
    
    - **`CadastrarProduto`**: Método para cadastrar um novo produto.
    
    - **`AtualizarProduto`**: Método para atualizar um produto existente.
    
    - **`RemoverProduto`**: Método para remover um produto do banco de dados.
    
    - **`CriarPedido`**: Método para cadastrar um novo pedido.
    
    - **`AdicionarItemCarrinho`**: Método para adicionar um produto ao pedido, atualizando a quantidade no banco e recalculando o total de preço na tabela `PEDIDOS`.

---
+++ GerenciamentoDeEstoque/GerenciamentoDeEstoque/Models/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoDeEstoque.Models
+{
+    class Produto
+    {
+        public string Nome { set; get; }
+        public int Quantidade { set; get; }
+        public int Codigo { set; get; }
+        public decimal Preco { set; get; }
+        public int Id { set; get; }
+
+        public Produto(string nome, int quantidade, int codigo, decimal preco)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Codigo = codigo;
+            Preco = preco;
+        }
+
+        public Produto(string nome, int quantidade, int codigo, decimal preco, int id)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Codigo = codigo;
+            Preco = preco;
+            Id = id;
+        }
+    }
+}
